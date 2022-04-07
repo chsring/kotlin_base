0a478c6f17db3ef0b93a3afe4ec8ed8a56bf803f
srwing <694177407@qq.com>
2022-04-07 21:23:16 +0800

    注解

--- src/main/kotlin/kt_33.kt
+++ src/main/kotlin/kt_33.kt
@@ -18,7 +18,27 @@ class PersonT {
 
 }
 
+@JvmOverloads //编译器生成一个java中的重载函数
+fun show(name: String, age: Int = 20, sex: Char = 'M') {
+
+}
+
+class MyObject {
+
+    companion object {
+        //java中调用时, MyObject.Companion.TARGET，在java中会创建这个 static Companion类
+        val TARGET = "黄石公园"
+
+        @JvmStatic   // java中会把 name 这个类 放到Companion类外面，所以可以 MyObject.name
+        val name = "sss"
+
+        fun showAction(name: String) = println("$name 要去 $TARGET 玩")
+    }
+}
 
 fun main() {
+    // java中 无法享用kt中的默认参数,除非在方法外 JvmOverloads注解
+    show("张三")
 
+    MyObject.showAction()
 }
